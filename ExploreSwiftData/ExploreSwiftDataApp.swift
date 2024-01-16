98a3ebf84f0ad22af1fce0ceb93eb42acd0db2b5
burakkurtarir <burak.kurtarir35@hotmail.com>
2024-01-16 11:54:53 +0300

    Initial Commit

---
+++ ExploreSwiftData/ExploreSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreSwiftDataApp.swift
+//  ExploreSwiftData
+//
+//  Created by Burak Kurtarır on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExploreSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
